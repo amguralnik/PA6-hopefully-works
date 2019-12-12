37bf47376b5be9db8983c1025cc112ea8c137317
amguralnik <55504856+amguralnik@users.noreply.github.com>
2019-12-12 12:03:42 -0600

    Add files via upload

---
+++ PA6/PA6/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace PA6
+{
+    partial class frmCWID
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCWID = new System.Windows.Forms.Label();
+            this.txtCWID = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCWID
+            // 
+            this.lblCWID.AutoSize = true;
+            this.lblCWID.Location = new System.Drawing.Point(12, 23);
+            this.lblCWID.Name = "lblCWID";
+            this.lblCWID.Size = new System.Drawing.Size(163, 24);
+            this.lblCWID.TabIndex = 0;
+            this.lblCWID.Text = "Enter Your CWID: ";
+            // 
+            // txtCWID
+            // 
+            this.txtCWID.Location = new System.Drawing.Point(169, 23);
+            this.txtCWID.Name = "txtCWID";
+            this.txtCWID.Size = new System.Drawing.Size(222, 29);
+            this.txtCWID.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(316, 84);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 32);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(169, 84);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 32);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // frmCWID
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 164);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtCWID);
+            this.Controls.Add(this.lblCWID);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmCWID";
+            this.Text = "CWID";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCWID;
+        private System.Windows.Forms.TextBox txtCWID;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ PA6/PA6/frmMain.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA6
+{
+    public partial class frmMain : Form
+    {
+        string cwid;
+        List<Book> myBooks;
+
+        public frmMain(string tempCwid)
+        {
+            this.cwid = tempCwid;
+            InitializeComponent();
+            pbCover.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            LoadList();
+        }
+
+        private void LoadList()
+        {
+            myBooks = BookFile.GetAllBooks(cwid);
+            lstBooks.DataSource = myBooks;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LstBooks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Book myBook = (Book)lstBooks.SelectedItem;
+
+            txtTitleData.Text = myBook.title;
+            txtAuthorData.Text = myBook.author;
+            txtGenreData.Text = myBook.genre;
+            txtIsbnData.Text = myBook.isbn;
+            txtCopiesData.Text = myBook.copies.ToString();
+            txtLengthData.Text = myBook.length.ToString();
+
+            try
+            {
+                pbCover.Load(myBook.cover);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void BtnRent_Click(object sender, EventArgs e)
+        {
+            Book myBook = (Book)lstBooks.SelectedItem;
+
+            myBook.copies--;
+            BookFile.SaveBook(myBook, cwid, "edit");
+            LoadList();
+        }
+
+        private void BtnReturn_Click(object sender, EventArgs e)
+        {
+            Book myBook = (Book)lstBooks.SelectedItem;
+
+            myBook.copies++;
+            BookFile.SaveBook(myBook, cwid, "edit");
+            LoadList();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            Book myBook = (Book)lstBooks.SelectedItem;
+
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete?", "Delete", MessageBoxButtons.YesNo);
+
+            if(dialogResult == DialogResult.Yes)
+            {
+                BookFile.DeleteBook(myBook, cwid);
+                LoadList();
+
+            }
+        }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            Book myBook = (Book)lstBooks.SelectedItem;
+            frmEdit myForm = new frmEdit(myBook, "edit", cwid);
+            if (myForm.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+            else
+            {
+                LoadList();
+            }
+        }
+
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+            Book myBook = new Book();
+            frmEdit myForm = new frmEdit(myBook, "new", cwid);
+            if (myForm.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+            else
+            {
+                LoadList();
+            }
+        }
+    }
+}
